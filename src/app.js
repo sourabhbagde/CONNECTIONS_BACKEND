7d18006e08010043ec98d2a5203b78a8f3eb75c8
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-10-31 04:29:02 -0500

    Added nodemon.

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const express = require('express');
 const app = express();
 
 // request handler
-app.use((req, res) => {
-    res.send("This is server response");
+app.use("/", (req, res) => {
+    res.send("This is server response.");
 });
 // start the server at port 3000
 app.listen(3000);
