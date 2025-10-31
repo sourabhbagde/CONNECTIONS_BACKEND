f7bb3689140a170f44174e79e2fc202375722156
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-10-31 04:19:02 -0500

    App commit.

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+// express server instance
+const app = express();
+
+// request handler
+app.use((req, res) => {
+    res.send("This is server response");
+});
+// start the server at port 3000
+app.listen(3000);
