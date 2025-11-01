48275992217daf4130ccb1e5a9d2182c3f3d1a08
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-11-01 01:52:41 -0500

    Request handlers.

--- src/app.js
+++ src/app.js
@@ -3,9 +3,19 @@ const express = require('express');
 // express server instance
 const app = express();
 
-// request handler
+// request handlers
+
+app.use('/first', (req, res) => {
+  res.send('This is first endpoint.');
+});
+
 app.use("/", (req, res) => {
     res.send("This is server response.");
 });
+
+app.use("/second", (req, res) => {
+    res.send("This is second endpoint.");
+})
+
 // start the server at port 3000
 app.listen(3000);
