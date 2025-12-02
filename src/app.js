6d3ba335b61c0158029a91a5d56f5ca9aea4fac8
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-12-02 06:39:56 -0600

    Writing middlewares for user and admin.

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ const express = require('express');
 // express server instance
 const app = express();
 
+// handling middlewares from separate file as authAdmin and authUser from auth.js
 const {authAdmin, authUser} = require('./middlewares/auth');
 
 // request handlers
 app.use('/admin', authAdmin);
 app.use('/user', authUser);
 
-app.get('/user/getInfo', (req, res) => {
+app.get('/user/getInfo', authUser, (req, res) => {
     res.send("User Info");
 });
 app.listen(3000);
