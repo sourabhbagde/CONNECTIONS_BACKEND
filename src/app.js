cdc6742ecd42953fe5b48f63332d7217018b2684
Sourabh Bagde <sourabh.bagde.1.sb@gmail.com>
2025-12-02 07:34:34 -0600

    Handling error with / to all routes.

--- src/app.js
+++ src/app.js
@@ -4,13 +4,24 @@ const express = require('express');
 const app = express();
 
 // handling middlewares from separate file as authAdmin and authUser from auth.js
-const {authAdmin, authUser} = require('./middlewares/auth');
+// const {authAdmin, authUser} = require('./middlewares/auth');
 
 // request handlers
-app.use('/admin', authAdmin);
-app.use('/user', authUser);
+// app.use('/admin', authAdmin);
+// app.use('/user', authUser);
 
-app.get('/user/getInfo', authUser, (req, res) => {
+// app.get('/user/getInfo', authUser, (req, res) => {
+//     res.send("User Info");
+// });
+app.get('/getUserInfo', (req, res) => {
+    // here error is not handled properly.
+    throw new Error("Simulated server error");
     res.send("User Info");
 });
+
+app.use('/', (err, req, res, next) => {
+  // here error is handled properly.
+  res.status(500).send('Internal Server Error: ' + err.message);
+});
+
 app.listen(3000);
